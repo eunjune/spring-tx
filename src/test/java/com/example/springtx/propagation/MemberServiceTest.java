3bf2cb3e456aef81870b4b3f0c5d873a6f3c4897
eunjune <iws1605@naver.com>
2022-11-05 21:29:23 +0900

    트랜잭션 전파 활용 - 복구 REQUIRED

--- src/test/java/com/example/springtx/propagation/MemberServiceTest.java
+++ src/test/java/com/example/springtx/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -98,6 +99,25 @@ class MemberServiceTest {
                 .isInstanceOf(RuntimeException.class);
 
         Assertions.assertTrue(memberRepository.find(username).isEmpty());
+        Assertions.assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService    @Transactional:ON
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON Exception
+     */
+    // 예외를 try catch로 처리해도 데이터가 다 롤백된다.
+    @Test
+    public void recoverException_fail() {
+
+        // given
+        String username = "로그예외_recoverException_fail";
+
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
+
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
         Assertions.assertTrue(logRepository.find(username).isEmpty());
     }
 }
